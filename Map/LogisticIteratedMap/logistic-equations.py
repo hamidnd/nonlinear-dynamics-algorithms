4c1ff5262a7bb24d6b3c851797b94260a966dba7
Topdown Bottomup Thinking <hamid.n.d@gmail.com>
2026-09-17 20:03:56 +0330

    Create logistic-equations.py

---
+++ Map/LogisticIteratedMap/logistic-equations.py
@@ -0,0 +1,32 @@
+# X_n+1 = R * X_n(1 - X_n) = R*X_n - R*X_n^2
+def logisticEquation(r, x0, ntime):
+    output = {}
+    x_next=x0
+    for i in range(ntime):
+        x_next= r * x_next * ( 1 - x_next)
+        output[i] = x_next        
+    return x_next
+
+def logisticEquation_trajectory(r, x0, ntime):
+    output = {}
+    x_next=x0
+    for i in range(ntime):
+        x_next= r * x_next * (1 - x_next)
+        output[i] = x_next        
+    return output
+
+def iterated_cosine(x0, ntime):    
+		import math
+    xnext = x0
+    for i in range(ntime):
+        xnext = math.cos(math.radians(xnext))
+    return xnext
+
+def iterated_cosine_trajectory(x0, ntime): 
+		import math
+    xnext = x0
+    traj=[]
+    for i in range(ntime):
+        xnext = math.cos(math.radians(xnext))
+        traj.append(xnext)
+    return traj
