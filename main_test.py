cb9fb9bce5763a7503079973fef8e25e9dc79ffb
hamidnafiyan <hamid.n.d@gmail.com>
2026-09-18 20:57:47 +0330

    - added main_test - added .gitignore

---
+++ main_test.py
@@ -0,0 +1,22 @@
+from flows.OrdinaryDifferentialEquationSolvers.SinglestepSolvers.ode_fieldVector import ode_derivativeof_fieldVector_SHO
+from flows.OrdinaryDifferentialEquationSolvers.SinglestepSolvers.ExplicitSolvers.forwardEuler_RK1 import forward_Euler_SHO_default, forward_Euler_SHO
+from flows.OrdinaryDifferentialEquationSolvers.SinglestepSolvers.ImplicitSolvers.backwardEuler_RK1 import backwardEuler_SHO
+
+
+# python main_test.py
+if __name__ == "__main__":
+
+    print(ode_derivativeof_fieldVector_SHO((-1, -2)))
+
+    print(forward_Euler_SHO_default((-1, -2), 0.1,  0.5, True))
+
+    print(forward_Euler_SHO_default((-1, -2), 0.2, 0.5, True))
+
+    print(forward_Euler_SHO_default((-1, -2), 0.1, 0.1)[0])
+
+    print(forward_Euler_SHO_default((-1, -2), 0.2, 1000, True)[2])
+
+    # k= 2, m= 1, g=0, betha =0
+    print(forward_Euler_SHO((-1, -2), 0.05, 0.5, 2, 1, 0, 0))
+    
+    print(backwardEuler_SHO((-1,-2), 0.1,0.5))
