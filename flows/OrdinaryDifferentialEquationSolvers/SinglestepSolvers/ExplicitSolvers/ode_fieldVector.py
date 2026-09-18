10161c948e392d037a573ac4abb46c3d86be82ee
hamidnafiyan <hamid.n.d@gmail.com>
2026-09-18 19:20:40 +0330

    default params for SHO

--- flows/OrdinaryDifferentialEquationSolvers/SinglestepSolvers/ExplicitSolvers/ode_fieldVector.py
+++ flows/OrdinaryDifferentialEquationSolvers/SinglestepSolvers/ExplicitSolvers/ode_fieldVector.py
@@ -3,7 +3,7 @@ def ode_derivativeof_fieldVector(xFieldVector, *params):
     pass 
 
 
-def ode_derivativeof_fieldVector_SHO(xFieldVector=(0,0), k= 2, m= 0.5, g=0, betha =0):
+def ode_derivativeof_fieldVector_SHO(xFieldVector=(0,0), k= 2.0, m= 0.5, g=0, betha =0):
     (x,v) = xFieldVector
     xPrime = v
     vPrime = g-(k/m)*x - (betha/m)*v
