31854da3039cc2be3852a1d875ab4d6670a303e9
hamidnafiyan <hamid.n.d@gmail.com>
2026-09-18 19:06:32 +0330

    Forward Euler is one of the Single-Step ODE Solvers: 1- implementation of Field Vector: Trajectory Nonlinearization through Fields Vector (Jacobian matrix) consider an ODE as a Field vector, telling us for every point in the state space the direction that is downhill, i.e., what the slop of the landscape is. 2- implementation of Forward Euler (Explicit Solver): The implementation is for an undamped system like an undamped 2-dimentional Simple harmonic oscillator (SHO) with 2 state variables X and V The solution for its ODE is a circle in state space, which is cosine and sine sequentially

---
+++ flows/OrdinaryDifferentialEquationSolvers/SinglestepSolvers/ExplicitSolvers/forwardEuler_RK1.py
@@ -0,0 +1,56 @@
+import ode_fieldVector
+
+def forward_Euler_SHO(ic, timestep, finalTimeStep, estimation_timecomplexity=False):
+    
+    # The implementation is for an undamped system like an undamped 2-dimentional Simple harmonic oscillator (SHO)
+    # with 2 state variables X and V
+    # The solution for its ODE is a circle in state space, which is cosine and sine sequentially
+    
+    trajectories = {}
+    trajectories[0] = ic
+    laststate = ic
+    iterations = int(finalTimeStep/timestep)
+    o_n = 0  # the number of iterations
+    
+    for i in range(iterations):
+        
+        # get the last field vector
+        fv = trajectories[i]  
+        
+        # our nearest guess
+        # parameter values are default for SHO
+        (xprime, vprime) = ode_fieldVector.ode_derivativeof_fieldVector_SHO(fv)
+        
+        directionslop = (timestep*xprime, timestep*vprime)
+        next_position = (directionslop[0]+fv[0], directionslop[1]+fv[1])
+        trajectories[i+1] = next_position
+        laststate = next_position
+        
+        # time execution order:
+        if estimation_timecomplexity:
+            o_n += 1            
+    return (laststate, trajectories, o_n)
+
+def forward_Euler_SHO(ic, timestep, finalTimeStep, *sho_args):
+    
+    trajectories={}
+    trajectories[0]=ic
+    laststate=ic
+    iterations=int(finalTimeStep/timestep)
+    o_n = 0 # the number of iterations
+    for i in range(iterations):
+        
+        # get the last field vector
+        fv=trajectories[i] 
+        
+        # our nearest guess 
+        # non-default parameters are taken from caller
+        (xprime,vprime) = ode_fieldVector.ode_derivativeof_fieldVector_SHO(fv, *sho_args) 
+        
+        directionslop=(timestep*xprime, timestep*vprime)
+        next_position=(directionslop[0]+fv[0], directionslop[1]+fv[1])
+        trajectories[i+1]=next_position
+        laststate=next_position        
+        o_n+=1        
+    return (laststate, trajectories, o_n)
+
---
+++ flows/OrdinaryDifferentialEquationSolvers/SinglestepSolvers/ExplicitSolvers/ode_fieldVector.py
@@ -0,0 +1,10 @@
+# abstraction for derivative implementation
+def ode_derivativeof_fieldVector(xFieldVector, *params):
+    pass 
+
+
+def ode_derivativeof_fieldVector_SHO(xFieldVector=(0,0), k= 2, m= 0.5, g=0, betha =0):
+    (x,v) = xFieldVector
+    xPrime = v
+    vPrime = g-(k/m)*x - (betha/m)*v
+    return (xPrime, vPrime)
