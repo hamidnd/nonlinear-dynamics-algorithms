f812087fc534add2e817128b0aecb7092302e406
hamidnafiyan <hamid.n.d@gmail.com>
2026-09-19 14:53:30 +0330

    edited

--- main_test.py
+++ main_test.py
@@ -6,17 +6,24 @@ from flows.OrdinaryDifferentialEquationSolvers.SinglestepSolvers.ImplicitSolvers
 # python main_test.py
 if __name__ == "__main__":
 
-    print('\n',ode_derivativeof_fieldVector_SHO((-1, -2)))    
+    print('\n','='*20,'ode_derivativeof_fieldVector_SHO:' )
+    print(ode_derivativeof_fieldVector_SHO((-1, -2)))    
 
+    print('\n','='*20,'forward_Euler_SHO_default:' )
+    print('\n',forward_Euler_SHO_default((-1, -2), 0.1, 1)[0])
+    
+    print('\n','='*20,'forward_Euler_SHO_default and IC at (-1, -2):' )
     print('\n',forward_Euler_SHO_default((-1, -2), 0.1,  0.5, True))
 
+    print('\n','='*20,'forward_Euler_SHO_default with bigger time step:' )
     print('\n',forward_Euler_SHO_default((-1, -2), 0.2, 0.5, True))
 
-    print('\n',forward_Euler_SHO_default((-1, -2), 0.1, 0.1)[0])
-
+    print('\n','='*20,'Time Complexity of forward_Euler_SHO with 1000 iterations:' )
     print('\n',forward_Euler_SHO_default((-1, -2), 0.2, 1000, True)[2])
 
     # k= 2, m= 1, g=0, betha =0
+    print('\n','='*20,'forward_Euler with customized SHO parameters:' )
     print('\n',forward_Euler_SHO((-1, -2), 0.05, 0.5, 2, 1, 0, 0))
     
+    print('\n','='*20,'backwardEuler_SHO with IC at (-1,-2)' )
     print('\n',backwardEuler_SHO((-1,-2), 0.1,0.5))
